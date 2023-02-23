ffd8a4bb853f072ba98587f7483ea3fb652165b2
alefra88 <efra.rlz@gmail.com>
2023-02-23 02:23:58 -0600

    ajax + axios

---
+++ app.js
@@ -0,0 +1,65 @@
+// (() => {
+//   const $fetchAsync = document.getElementById("fetch-async"),
+//     $fragment = document.createDocumentFragment();
+//   async function getData() {
+//     try {
+//       let res = await fetch("https://rickandmortyapi.com/api/location"),
+//         { results: characters } = await res.json();
+//       if (!res.ok) throw { status: res.status, statusText: res.statusText };
+//       characters.forEach((el) => {
+//         const article = document.createRange().createContextualFragment(`
+//       <article>
+//       <div class="container">
+//         <h2> location name: ${el.name}</h2>
+//         <h3>type: ${el.type}</h3>
+//         <h3>dimension: ${el.dimension}</h3>
+//       </div>
+//       </article>
+//       <br>
+//       <hr>
+//     `);
+//         $fragment.append(article);
+//       });
+//       $fetchAsync.append($fragment);
+//     } catch (err) {
+//       console.log(err);
+//       let message = err.statusText || "Error desconocido";
+//       $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
+//     } finally {
+//     }
+//   }
+//   getData();
+// })();
+
+(() => {
+  const $axios = document.getElementById("axios"),
+    $fragment = document.createDocumentFragment();
+  axios
+    .get("https://rickandmortyapi.com/api/location")
+    .then((res) => {
+      console.log(res);
+      let json = res.data.results;
+      json.forEach((el) => {
+        const article = document.createRange().createContextualFragment(`
+              <article>
+              <div class="container">
+                <h2> location name: ${el.name}</h2>
+                <h3>type: ${el.type}</h3>
+                <h3>dimension: ${el.dimension}</h3>
+              </div>
+              </article>
+              <br>
+              <hr>
+            `);
+        $fragment.append(article);
+      });
+      $axios.append($fragment);
+    })
+    .catch((err) => {
+      let message = err.response.statusText || "Error aquì -->";
+      $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+    })
+    .finally(() => {
+      console.log("esto se ejecutarà independientemente del resultado");
+    });
+})();
